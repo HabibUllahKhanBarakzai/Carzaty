7559e448ec82016658c49017f72427b41bbd98c0
Habib Ullah Khan <ayubi.pakistani@gmail.com>
2019-10-31 19:59:30 +0500

    second commit -m

--- carzaty_machine_learning_algorithm/building_model.py
+++ carzaty_machine_learning_algorithm/building_model.py
@@ -1,6 +1,6 @@
 from keras.layers import Dense, Flatten, Dropout
 from keras.models import Sequential
-from keras.optimizers import Adam, SGD, RMSprop
+from keras.optimizers import Adam, SGD, RMSprop, Adadelta, Adagrad, Adamax
 from keras.layers.advanced_activations import LeakyReLU
 
 from applying_one_hot_encoding import one_hot_encoder_applied
@@ -8,27 +8,19 @@ from applying_one_hot_encoding import one_hot_encoder_applied
 X, y = one_hot_encoder_applied()
 
 model = Sequential()
-model.add(Dense(1000, input_shape=X.shape[1:], activation="relu"))
-model.add(Dropout(0.15))
-model.add(Dense(500,  activation="relu"))
-model.add(Dropout(0.12))
-model.add(Dense(500,  activation="relu"))
-model.add(Dropout(0.1))
+model.add(Dense(500, input_shape=X.shape[1:], activation="relu"))
 model.add(Dense(220,  activation="relu"))
-model.add(Dropout(0.1))
-model.add(Dense(200,  activation="relu"))
-model.add(Dropout(0.1))
+model.add(Dense(220,  activation="relu"))
 model.add(Dense(100, activation="relu"))
-model.add(Dropout(0.1))
-model.add(Dense(1))
+model.add(Dense(1,))
 
-optimizer = Adam(learning_rate=0.01, decay=0.01)
+optimizer = Adamax(learning_rate=0.01)
 
 model.compile(optimizer=optimizer,
-              metrics=["mape"],
+              metrics=["mae"],
               loss="mean_squared_logarithmic_error")
 
 model.fit(X, y,
-          epochs=100, validation_split=0.15)
+          epochs=5000)
 
 model.save("car_prediction_model_1.h5")
